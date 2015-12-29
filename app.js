42037a0221747bb2313a1f5691f958cf5ecde4b4
palessandri <pedro@fourhats.com.ar>
2015-12-28 23:04:24 -0300

    full commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ0VXE3cTZNNEp6S2R1a2h5eTVUbU0zRTZ2cVRVN3NBMCIsInNjb3BlcyI6eyJjbGllbnRzIjp7ImFjdGlvbnMiOlsicmVhZCJdfSwicnVsZXMiOnsiYWN0aW9ucyI6WyJyZWFkIl19fSwiaWF0IjoxNDUxMzUwODIwLCJqdGkiOiJiNWE4YTI3ZjA1ZjQyYjg3ODk0OGU5NDA4MzkwOTNjNiJ9.j6xZc0VbfXa_VIkBjbbhJgSX_ApEVm6CQA19o4i7ppY";
     var lock = new Auth0Lock(
       // All these properties are set in auth0-variables.js
       AUTH0_CLIENT_ID,
@@ -16,18 +17,16 @@ $(document).ready(function() {
           alert("There was an error logging in");
         } else {
           // Success calback
-
-          // Save the JWT token.
-          localStorage.setItem('userToken', token);
-
-          // Save the profile
-          userProfile = profile;
-
-          $('.login-box').hide();
-          $('.logged-in-box').show();
-          $('.nickname').text(profile.nickname);
-          $('.nickname').text(profile.name);
-          $('.avatar').attr('src', profile.picture);
+		  $('.login-box').hide();
+		  $('.logged-in-box').show();
+		  $.ajax({
+			url: 'https://pedroalessandri.auth0.com/api/v2/rules',
+			method: 'GET'
+		  }).then(function(data, textStatus, jqXHR) {
+			writeRules(data);
+		  }, function() {
+			alert("You need to download the server seed and start it to call this API");
+		  });
         }
       });
     });
@@ -36,22 +35,24 @@ $(document).ready(function() {
       'beforeSend': function(xhr) {
         if (localStorage.getItem('userToken')) {
           xhr.setRequestHeader('Authorization',
-                'Bearer ' + localStorage.getItem('userToken'));
+                'Bearer ' + token);
         }
       }
     });
+	
+	function writeRules(allRules) {
+		allRules.forEach(function(rule) {
+		applications = rule.script.substring(rule.script.search("Applications") + 13, rule.script.search(";"));
+		var applications = applications.split(',');
+  	    var dataContent = document.getElementById('dataContent');
+		dataContent.innerHTML = dataContent.innerHTML + "<h4>Rule name: " + rule.name + "<h4><br>";
+		applications.forEach(function(application) {
+			dataContent.innerHTML = dataContent.innerHTML + "<p>" + application + "</p><br>"
+		});
+		});
+};
 
-    $('.btn-api').click(function(e) {
-      // Just call your API here. The header will be sent
-      $.ajax({
-        url: 'http://localhost:3001/secured/ping',
-        method: 'GET'
-      }).then(function(data, textStatus, jqXHR) {
-        alert("The request to the secured enpoint was successfull");
-      }, function() {
-        alert("You need to download the server seed and start it to call this API");
-      });
-    });
+
 
 
 });
