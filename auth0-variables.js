ce3943599c6020fd2f06fb8fe928eca3571fcebe
pedrofourhats <pedro@fourhats.com.ar>
2015-12-29 21:47:58 -0300

    Update auth0-variables.js

--- auth0-variables.js
+++ auth0-variables.js
@@ -1,3 +1,5 @@
+//This information must be replaced with your account information. Once you are logged in auth0.com, go to https://manage.auth0.com/#/applications and create a new jquery based application. 
+//Select the settings section and there you will find your cliendID and domain info.
 var AUTH0_CLIENT_ID='40yVoMwDhSBkeYFa1Gs0CqnnOcmzM2EI'; 
 var AUTH0_DOMAIN='pedroalessandri.auth0.com'; 
 var AUTH0_CALLBACK_URL=location.href;
